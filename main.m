48a674f8dcb6ac4c02655b871a725aa5e96d6fea
logan4dog@gmail.com <logan4dog@gmail.com>
2026-01-04 10:14:28 -0500

    first commit

---
+++ main.m
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog (@"Programming is fun!");
+    }
+    return 0;
+}
